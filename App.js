ced9a02831713f5cf8d34ef96f55ea05401c22fd
Kiyo Kanno <kiyotakakanno2010@gmail.com>
2020-08-28 22:08:01 +1000

    Delete Image from state in App.js

--- App.js
+++ App.js
@@ -1,12 +1,7 @@
-  // import { StatusBar } from 'expo-status-bar';
-  // import React, {Component} from 'react'; //Using class ->{Component}
-  // import { StyleSheet, Text, View, SafeAreaView, FlatList, TextInput, TouchableOpacity } from 'react-native';//Add Flatlist
-  
   /* Added for code cleaning*/
   import { StatusBar } from 'expo-status-bar'
   import React, { Component } from 'react'
   import {
-   // StyleSheet,
     Text,
     View,
     SafeAreaView,
@@ -14,14 +9,14 @@
     TextInput,
     TouchableOpacity,
     Button,
-   // AsyncStorage,
-    //Platform,
+    //AsyncStorage, // Not using for own coding
+    //Platform,      // Not using for own coding
   } from 'react-native'
   /* End ------Added for code cleaning*/
   // third party components
   import RNPickerSelect from 'react-native-picker-select'
   // custom components
-  import { Item } from './components/Item' // How to import another js file.
+  import { Item } from './components/Item'
   // stylesheets
   import {styles} from './styles/Main';
   import {pickerStyle} from './styles/Picker';
@@ -38,7 +33,6 @@
       expenseAmount: '',
       expenseCategory: '',
       validInput: false, 
-      image: require('./assets/checklist-icon.png'),//testing for setting an image as a state
     }
     listData = []
 
@@ -59,7 +53,7 @@
       return (
         <SafeAreaView style={{flex:1}}>
           <View style={styles.main}>
-            <Text style ={styles.title}>- To Do List / Expense Tracker -</Text>
+            <Text style ={styles.title}>- Easy To Do List / Expense Tracker -</Text>
             <TextInput
               style={styles.input}
               placeholder="$ amount"
@@ -128,15 +122,15 @@
       />
     )
 
+    // Meant to be changin the exclamation image to ticked mark image by clicking
     tickTheBox = ( itemId ) => {
       this.listData.forEach ( (item,index) => {
         if( item.id == itemId ) {
-          //this.listData.splice( index, 1 )
           item.status = true
           this.setState({image: './assets/checklist-icon.png'})//calling an image object works?
         }
       })
-     // this.saveList()
+     // this.saveList()  Not using these codes below for own desgin purpose
         this.setState({expenseAmount:''})// Changed from 0 -> '' so that int 0 can be entered.
     }
 
@@ -148,6 +142,8 @@
       } )
       this.setState({expenseAmount:''})// Changed from 0 -> '' so that int 0 can be entered.
     }   
+
+    //Not using these codes below for own desgin purpose
     //   this.saveList()
     //   this.setState({expenseAmount:0})
     // }
@@ -189,101 +185,4 @@
         return item2.id - item1.id
       } )
     }
-
-    // saveList = async () => {
-    //   try {
-    //     await AsyncStorage.setItem(
-    //       'data',
-    //       JSON.stringify(this.listData)
-    //     )
-    //   }
-    //   catch( error ) {
-    //     console.log(error)
-    //   }
-    // }
-  
-    // loadList = async () => {
-    //   try{
-    //     let items = await AsyncStorage.getItem('data')
-    //     this.listData = JSON.parse( items )
-    //     this.setState({expenseAmount:0})
-    //   }
-    //   catch(error) {
-    //     console.log(error)
-    //   }
-    // }
   }
-
-  // const colors = {
-  //   primary : '#03fcba',//'hsla(330, 38%, 65%, 1)',
-  //   primaryDisabled : '#eb3486',
-  // }
-
-  // const pickerPlaceholder = {
-  //   label: 'select category',
-  //   value: null,
-  //   color: 'black'
-  // }
-
-  // const styles = StyleSheet.create({
-  //   main: {
-  //     paddingHorizontal: 10,
-  //   },
-  //   input: {
-  //     width:'100%',
-  //     padding: 10,
-  //     borderColor: 'black',
-  //     borderWidth: 1,
-  //     marginVertical: 15
-  //   },
-  //   button: {
-  //     marginTop: 10,
-  //     padding: 20,
-  //     backgroundColor: colors.primary,
-  //     marginVertical: 15
-  //   },
-  //   buttonText: {
-  //     fontSize: 20, 
-  //     fontWeight: 'bold',
-  //     color: 'white',
-  //     textAlign: 'center'
-  //   },
-  //   buttonDisabled:{
-  //     marginTop: 10,
-  //     padding: 20,
-  //     backgroundColor: colors.primaryDisabled,
-  //     marginVertical: 15
-  //   },
-  // })
-
-  // const pickerStyle = StyleSheet.create({
-  //   inputIOS:{
-  //   padding: 10,
-  //   borderColor: 'black',
-  //   borderWidth: 1,
-  // },
-  //   inputAndroid: {
-  //     padding: 10,
-  //     borderColor: 'black',
-  //     borderWidth: 1,
-  //   }
-  // })
-
-  /*** Default ***/
-  // export default function App() {
-  //   return (
-  //     <View style={styles.container}>
-  //       <Text>Open up App.js to start working on your app!</Text>
-  //       <StatusBar style="auto" />
-  //     </View>
-  //   );
-  // }
-
-  // const styles = StyleSheet.create({
-  //   container: {
-  //     flex: 1,
-  //     backgroundColor: '#fff',
-  //     alignItems: 'center',
-  //     justifyContent: 'center',
-  //   },
-  // });
